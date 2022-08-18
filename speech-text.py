7daa210c70158eabc59650af6a4f3263a1718828
Fardin <60337534+FardinHash@users.noreply.github.com>
2022-08-19 02:03:26 +0600

    Update speech-text.py

--- speech-text.py
+++ speech-text.py
@@ -1,1 +1,15 @@
 
+import speech_recognition as sr
+
+r = sr.Recognizer()
+
+with sr.AudioFile('I-dont-know.wav') as source:
+    audio_text = r.listen(source)
+    try:
+        text = r.recognize_google(audio_text)
+        
+        print('Converting speech..')
+        print(text)
+        
+    except:
+         print('Didnt get that, say again!')
